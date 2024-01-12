17de515bfc39eaac3ebed081902e64e39c681d10
Paulo Vidal <paulo.vidal@bitfy.app>
2024-01-12 19:26:22 -0300

    chore: deps / +@nestjs/config && feat: example GET chainid route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JsonRpcProvider } from 'ethers';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getChainId(): Promise<string> {
+    const provider = new JsonRpcProvider(process.env.NODE_URL);
+    const network = await provider.getNetwork();
+    const chainId = network.chainId;
+    return chainId.toString();
   }
 }
